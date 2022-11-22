a5e71590b7ca87cfbddac92ad4cb6967bb2e7199
Kasper-coder <kasper.holgersson@elev.ga.ntig.se>
2022-11-22 14:32:02 +0100

    Add an automated testing template from visual studio
    Co-Authored-By: Leon Lööv <111981993+Leon-Loov@users.noreply.github.com>
    Co-Authored-By: Leo Kempe <leo.max.kempe@gmail.com>
    Co-Authored-By: ViggoBlomqvist <111963633+Viggoblom@users.noreply.github.com>

---
+++ Automated-testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Automated-testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Academedia")]
+[assembly: AssemblyProduct("Automated-testing")]
+[assembly: AssemblyCopyright("Copyright © Academedia 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9602dec4-e5db-4a3c-b672-947d49f7b665")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Automated-testing/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Automated_testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
