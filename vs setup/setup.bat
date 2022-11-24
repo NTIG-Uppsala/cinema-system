5b6239b376fe4afdc5563226eec6635bf5772031
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-11-24 09:31:06 +0100

    Create folder with automatic setup script
    The script will install and configure visual studio community

---
+++ vs setup/setup.bat
@@ -0,0 +1,4 @@
+".\vs_Community.exe" --config "%~dp0.vsconfig" --installPath "C:\Program Files\Microsoft Visual Studio\2022\Community" --force^
+ --add Microsoft.VisualStudio.Workload.ManagedDesktop^
+ --add Microsoft.VisualStudio.Workload.NetCrossPlat^
+ --add Microsoft.VisualStudio.Workload.Universal
