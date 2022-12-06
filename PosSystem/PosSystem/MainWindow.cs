cf92fff5909687f992124a1fbe66181d454aa5bb
Kasper-coder <kasper.holgersson@elev.ga.ntig.se>
2022-12-06 12:02:51 +0100

    Add so items now stack in the listbox
    Co-Authored-By: ViggoBlomqvist <111963633+Viggoblom@users.noreply.github.com>

--- PosSystem/PosSystem/MainWindow.cs
+++ PosSystem/PosSystem/MainWindow.cs
@@ -1,9 +1,13 @@
+using System.Windows.Forms;
+
 namespace PosSystem
 {
     public partial class MainWindow : Form
     {
         private ProductClass _popcorn = new("Popcorn", 20m);
         private ProductClass _soda = new("Läsk", 20m);
+        public int PopcornAdd = 1;
+        public int SodaAdd = 1;
 
 
         public MainWindow()
@@ -11,37 +15,43 @@ namespace PosSystem
             InitializeComponent();
         }
 
+        // Add popcorn to checkout list
         private void PopcornButton_Click(object sender, EventArgs e)
         {
-            var i = 2;
-            var b = i++ + "x Popcorn";
-            // https://stackoverflow.com/questions/36236956/c-sharp-replace-text-in-listbox //
-            if (CheckoutList.Items.Cast<string>().Contains("1x Popcorn"))
-            {
-                this.TotalText.Text = Program.CheckoutList.Sum(x => x.product.Price).ToString() + "kr";
-                int a = CheckoutList.Items.IndexOf("1x Popcorn");
-                CheckoutList.Items.RemoveAt(a);
-                CheckoutList.Items.Add(b);
-            }
-            else
-            {
-                Program.AddToCheckout(_popcorn);
-                CheckoutList.Items.Add("1x Popcorn");
-                this.TotalText.Text = Program.CheckoutList.Sum(x => x.product.Price).ToString() + "kr";
-            }
-        }
-        
-        private void SodaButton_Click(object sender, EventArgs e)
-        {
-            Program.AddToCheckout(_soda);
-            this.CheckoutList.Items.Add("1x Läsk");
+            var a = PopcornAdd++ + "x Popcorn";
+            Program.AddToCheckout(_popcorn);
+            this.CheckoutList.Items.Add(a);
+            // Calculates total price and adds the text top the TotalText textbox
             this.TotalText.Text = Program.CheckoutList.Sum(x => x.product.Price).ToString() + "kr";
+
+            if (PopcornAdd > 2)
+            {
+                this.CheckoutList.Items.RemoveAt(this.CheckoutList.SelectedIndex != -1 ? this.CheckoutList.SelectedIndex : 0);
+            }
         }
 
+        // Add soda to checkout list
+        private void SodaButton_Click(object sender, EventArgs e)
+        {
+            var b = SodaAdd++ + "x Läsk";
+            Program.AddToCheckout(_soda);
+            this.CheckoutList.Items.Add(b);
+            // Calculates total price and adds the text top the TotalText textbox
+            this.TotalText.Text = Program.CheckoutList.Sum(x => x.product.Price).ToString() + "kr";
+
+            if (SodaAdd > 2)
+            {
+                this.CheckoutList.Items.RemoveAt(this.CheckoutList.SelectedIndex != -1 ? this.CheckoutList.SelectedIndex : 0);
+            }
+        }
+
+        // Clear the checkout list
         private void Clear_Click(object sender, EventArgs e)
         {
             Program.ClearCheckout();
             this.CheckoutList.Items.Clear();
+            PopcornAdd = 1;
+            SodaAdd = 1;
             this.TotalText.Text = "0kr";
         }
     }
