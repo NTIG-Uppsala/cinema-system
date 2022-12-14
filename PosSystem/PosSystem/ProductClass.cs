2b23e6e795ee8e5901ce9f291a373ea17e01bbf4
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-12-14 14:28:17 +0100

    Add optional VAT argument to products
    Co-Authored-By: ViggoBlomqvist <111963633+Viggoblom@users.noreply.github.com>

--- PosSystem/PosSystem/ProductClass.cs
+++ PosSystem/PosSystem/ProductClass.cs
@@ -11,10 +11,14 @@ namespace PosSystem
         public string Name = "";
         public decimal Price = 0m;
 
-        public ProductClass(string name, decimal price)
+        // VAT = moms and is 12% for food and 25% for most other products
+        public int Vat = 0;
+
+        public ProductClass(string name, decimal price, int vat = 12)
         {
             this.Name = name;
             this.Price = price;
+            this.Vat = vat;
         }
     }
 }
