546a4ce3df68f7c473e512c0662912810cc3b5db
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-01-09 14:48:47 +0100

    Start creating a system for creating products from file
    Co-Authored-By: ViggoBlomqvist <111963633+Viggoblom@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>

---
+++ PosSystem/PosSystem/Products.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosSystem
+{
+    public class Products
+    {
+        private static string csvLocation = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PosSystem\products.csv";
+
+        public List<ProductClass> productList = File.ReadAllLines(csvLocation)
+                                                    .Skip(1)
+                                                    .Select(v => ParseCsv(v))
+                                                    .ToList();
+
+        public static ProductClass ParseCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(",");
+
+            ProductClass product = new(values[0], decimal.Parse(values[1]), int.Parse(values[2]));
+
+            return product;
+        }
+    }
+}
