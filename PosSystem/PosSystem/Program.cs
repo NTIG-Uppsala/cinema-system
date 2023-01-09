897ae3ad1320f761a84abf7343c0ed9b2325e277
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-01-09 14:47:36 +0100

    Add a function to create folders and products if missing during startup
    Co-Authored-By: ViggoBlomqvist <111963633+Viggoblom@users.noreply.github.com>
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>

--- PosSystem/PosSystem/Program.cs
+++ PosSystem/PosSystem/Program.cs
@@ -31,6 +31,7 @@ namespace PosSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            new SetupFolders();
             Application.Run(new MainWindow());
         }
 
---
+++ PosSystem/PosSystem/SetupFolders.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosSystem
+{
+    public class SetupFolders
+    {
+        string baseFolder = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PosSystem";
+        string receiptFolder = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PosSystem\PosReceipts";
+
+        public SetupFolders()
+        {
+            Directory.CreateDirectory(baseFolder);
+            Directory.CreateDirectory(receiptFolder);
+
+            if (!File.Exists($@"{baseFolder}\products.csv"))
+            {
+                List<string> products = new List<string>()
+                {
+                    "Popcorn Liten,25,",
+                    "Popcorn Mellan,35,",
+                    "Popcorn Stor,50,",
+                    "Läsk Liten,20,",
+                    "Läsk Mellan,25,",
+                    "Läsk Stor,35,",
+                };
+
+                File.WriteAllLines($@"{baseFolder}\products.csv", products);
+            }
+        }
+    }
+}
