2306f99ffd2b4a309124796c4fadd9d0c136a60c
Axel Schnürer <axel.schnurer@elev.ga.ntig.se>
2023-01-18 10:16:18 +0100

    Generate database with tables

--- PosSystem/PosSystem/SetupFolders.cs
+++ PosSystem/PosSystem/SetupFolders.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
 using Microsoft.Data.Sqlite;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace PosSystem
@@ -14,6 +19,18 @@ namespace PosSystem
     {
         string baseFolder = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PosSystem";
         string receiptFolder = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PosSystem\PosReceipts";
+
+        private static readonly string[] tables =
+        {           
+            "CREATE TABLE products (name text, price text, vat integer, product_id integer PRIMARY KEY AUTOINCREMENT, color text);",
+            "CREATE TABLE movies(name text, price text, vat integer,movie_id integer PRIMARY KEY AUTOINCREMENT, duration integer);",
+            "CREATE TABLE reservation (seat_id integer, reservation_id integer PRIMARY KEY AUTOINCREMENT, screening_id integer, booking_id integer);",
+            "CREATE TABLE screenings (movie_id integer, start_time text, screen_id integer PRIMARY KEY AUTOINCREMENT, salon_id integer);",
+            "CREATE TABLE salon (seats integer, seat_amount integer, salon_id integer PRIMARY KEY AUTOINCREMENT);",
+            "CREATE TABLE seats (seat_id integer PRIMARY KEY AUTOINCREMENT, number integer, row integer, salon_id integer);",
+            "CREATE TABLE bookings (booking_id integer PRIMARY KEY AUTOINCREMENT, customer_id integer);",
+            "CREATE TABLE customer (customer_id integer PRIMARY KEY AUTOINCREMENT, name text);"
+        };
 
         public SetupFolders()
         {
@@ -48,19 +65,22 @@ namespace PosSystem
 
                 File.WriteAllLines($@"{baseFolder}\movies.csv", products);
             }
+            
 
             var dbPath = $@"{baseFolder}\database.db";
 
             if (!File.Exists(dbPath))
             {
-                string query = "CREATE TABLE bookings(booking_id int NOT NULL UNIQUE, PRIMARY KEY (booking_id))";
 
                 var db = new SqliteConnection($"Data Source={dbPath};");
                 db.Open();
 
                 using (DbCommand cmd = db.CreateCommand())
                 {
-                    cmd.CommandText += query;
+                    foreach (string table in tables)
+                    {
+                        cmd.CommandText += table;
+                    }
                     cmd.ExecuteReader();
                 }
                 db.Close();
